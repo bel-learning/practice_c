4446d3edebbe3386d2427de3ebc50c0b575b7cea
baljibil <baljibil@fit.cvut.cz>
2022-11-13 14:39:13 +0100

    hw5: Input figured out

--- homeworks/5/main.c
+++ homeworks/5/main.c
@@ -7,50 +7,77 @@ typedef struct Points {
     double y;
 } point;
 
-void getPlaneInput(point ** coordinates, point coordinate, char *** names, char * name, size_t *size, size_t *capacity) 
+void stripString(char * s) {
+    char * d = s;
+    int inSpaces = 1;
+    int howManyTimesWeChangedD = 0;
+    while (*s) {
+        if (inSpaces) {
+            if (*s != ' ') {
+                inSpaces = 0;
+                *d ++ = *s ++;
+                howManyTimesWeChangedD++;
+            } else {
+                 s ++;
+            }
+        } else {
+            if (*s == ' ') {
+                inSpaces = 1;
+            }
+            *d ++ = *s ++;
+            howManyTimesWeChangedD ++;
+        }
+    }
+    if (howManyTimesWeChangedD > 0 && inSpaces) d --;
+    *d = '\0';
+}
+
+
+void getPlaneInput(double ** coordinates, point coordinate, char *** names, char * name, size_t *size, size_t *capacity) 
 {
-    printf("%lf %lf\n", coordinate.x, coordinate.y);
+    // printf("%lf %lf\n", coordinate.x, coordinate.y);
+    // printf("sizeof %lu \n", sizeof(point));
     if (*size >= (*capacity) - 1)
     {
         (*capacity) = (*capacity) * 2;
-        *coordinates = (point *) realloc(*coordinates, (*capacity) * sizeof(*coordinates));
+        *coordinates = (double *) realloc(*coordinates, (*capacity*2) * sizeof(*coordinates));
         if(*coordinates == NULL) {
             printf("couldn't assign pointers\n");
         }
         *names = (char **) realloc(*names, (*capacity) * sizeof(*names));
-        printf("capacity: %lu\n", *capacity);
+        // printf("capacity: %lu\n", *capacity);
         for (int i = (*capacity)/2; i < (*capacity); i++)
         {
             (*names)[i] = (char *) malloc((MAX_STRING_LENGTH+1) * sizeof(char));   
         }
     }
-    printf("coordinate assigning\n");
-    printf("on size %lu\n", *size);
-    (*coordinates)[*size] = coordinate;
+    // printf("coordinate assigning\n");
+    // printf("on size %lu\n", *size);
+    
+    (*coordinates)[*size*2] = coordinate.x;
+    (*coordinates)[*size*2+1]=coordinate.y;
 
-    printf("coordinate assigned\n");
+    // printf("coordinate assigned\n");
 
     int i = 0;
     while(*name) {
-        printf("%c\n", *name++);
-        *names[*size][i] = *name++;
+        (*names)[*size][i++] = *name++;
     }
-    
+    (*names)[*size][i] = '\0';
     (*size)++;
     return;
 }
 
 int main()
 {
-    point *coordinates = (point *)malloc(100 * sizeof(point));
-
+    double *coordinates = (double *)malloc((2*100) * sizeof(*coordinates));
     char **names = (char **) malloc(100 * sizeof(char*));
     for (int i = 0; i < 100; i++)
-        names[i] = (char *) malloc((MAX_STRING_LENGTH) * sizeof(char)); 
+        names[i] = (char *) malloc((MAX_STRING_LENGTH + 1) * sizeof(char)); 
     
     point tmpPoint;
     char tmpName[MAX_STRING_LENGTH];
-    size_t size = 100;
+    size_t size = 0;
     size_t capacity = 100;
     printf("Plane coordinates:\n");
     while (1)
@@ -59,11 +86,18 @@ int main()
         if(x == EOF) {
             break;
         }
+        // stripString(tmpName);
         getPlaneInput(&coordinates, tmpPoint, &names, tmpName, &size, &capacity);
     }
-    for(int i = 0; i < 2; i++) {
-        printf("%s\n", names[i]);
-    }   
- 
+    for(size_t i = 0; i < size; i++) {
+        printf("%lf %lf\n ", coordinates[i*2], coordinates[i*2+1]);
+        char * pls = names[i];
+        
+        while(*pls) {
+            printf("%c", *pls ++);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
