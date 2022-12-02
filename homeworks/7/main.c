a0929827b23ef5bd1944627acb7b32d7a6ff6b30
baljibil <baljibil@fit.cvut.cz>
2022-12-02 18:51:29 +0100

    hw07: good but slow one

--- homeworks/7/main.c
+++ homeworks/7/main.c
@@ -94,58 +94,69 @@ twoValue search(ArrayWithSize * tokens, size_t size, int a, int b, int aTurn, in
     }
 }
 
-int insertArray(char *str, bool *colon, bool *brace1, bool *brace2, ArrayWithSize *tokens, int direction, int *index)
+int insertArray(char **str, bool *colon, bool *brace1, bool *brace2, ArrayWithSize *tokens, int direction, int *index)
 {
-    str++;
+    (*str)++;
     while (*str)
     {
+        // printf("Stuck: %c\n", **str);
         // printf(">%c<\n", *str);
-        if (*str == ' ')
+        while (**str == ' ')
         {
-            str++;
+            (*str)++;
             continue;
         }
-        if (*str == ':')
+        if (**str == ':')
         {
             (*colon) = true;
-            str++;
+            (*str)++;
             continue;
         }
-        if (*str == '{')
+        if (**str == '{')
         {
             (*brace1) = true;
-            str++;
+            (*str)++;
             continue;
         }
-        if (isdigit(*str) || *str == '-')
+        if (isdigit(**str) || **str == '-')
         {
             // hopefully number won't be higher than 100 digits lmao
             char *numStr = (char *)malloc(100);
             int cnt = 0;
-            while (isdigit(*str) || *str == '-')
+            while (isdigit(**str) || **str == '-')
             {
-                numStr[cnt++] = *str;
-                str++;
+                numStr[cnt++] = **str;
+                (*str)++;
             }
-            while (isspace(*str)) {
-                str++;
+            numStr[cnt] = '\0';
+            while (isspace(**str))
+            {
+                (*str)++;
             }
-            if (*str != ',' && *str != '}')
+            if (**str != ',' && **str != '}')
             {
                 // throw error
                 return 0;
             }
             int num = atoi(numStr);
+            // printf("num*Str: %s\n", numStr);
+            // printf("num: %d\n", num);
             tokens[direction].arr[(*index)] = num;
             (*index)++;
 
-            if(*str != '}') {
-                str++;
+            if (**str != '}')
+            {
+                (*str)++;
             }
 
             free(numStr);
         }
-        if (*str == '}')
+        while (**str == ' ')
+        {
+            (*str)++;
+            continue;
+        }
+        if (**str == '}')
         {
             (*brace2) = true;
             break;
@@ -154,79 +165,236 @@ int insertArray(char *str, bool *colon, bool *brace1, bool *brace2, ArrayWithSiz
     return 1;
 }
 
-void freeArray(ArrayWithSize ** tokens) {
-    for(size_t i = 0; i < 4; i++) {
-            free((*tokens)[i].arr);
-        }
+void freeArray(ArrayWithSize **tokens)
+{
+    for (size_t i = 0; i < 4; i++)
+    {
+        free((*tokens)[i].arr);
+    }
     free((*tokens));
 }
 
 int main()
 {
 
-    ArrayWithSize * tokens = (ArrayWithSize *) malloc(4 * sizeof(*tokens));
-    for(size_t i = 0; i < 4; i++) {
-        tokens[i].arr = (int *) malloc(sizeof(int) * 10);
+    ArrayWithSize *tokens = (ArrayWithSize *)malloc(4 * sizeof(*tokens));
+    for (size_t i = 0; i < 4; i++)
+    {
+        tokens[i].arr = (int *)malloc(33 * sizeof(int));
     }
-    tokens[0].arr[0] = 1;
-    tokens[0].arr[1] = 2;
-    tokens[0].size = 2;
-    
-    tokens[1].arr[0] = 3;
-    tokens[1].arr[1] = 5;
-    tokens[1].size = 2;
-    
-    tokens[2].arr[0] = 9;
-    tokens[2].arr[1] = 1;
-    tokens[2].arr[2] = 1;
-    tokens[2].arr[2] = 1;
-    tokens[2].size = 4;
+    ArrayWithSize **cTokens = &tokens;
+    printf("Tokens:\n");
 
-    tokens[3].arr[0] = 1;
-    tokens[3].arr[1] = 7;
-    tokens[3].size = 2;
-    
-    size_t tokenSize = 4+2+2+2;
+    int wi = 0;
+    int ni = 0;
+    int si = 0;
+    int ei = 0;
+
+    int tokenSize = 0;
+    char *str = NULL;
+    size_t size = 0;
+    bool inN, inW, inE, inS = false;
+    while (getline(&str, &size, stdin) != EOF)
+    {
+        char ** strAddress = &str;        
+        removeNL(str);
+        while (*str == ' ')
+        {
+            str++;
+            continue;
+        }
+        while (*str)
+        {
+            if (*str == 'N' || *str == 'W' || *str == 'E' || *str == 'S')
+            {
+                // read input
+                // printf(">%c<\n", *str);
+                if (*str == 'N')
+                {
+                    inN = true;
+                    bool colon = false, brace1 = false, brace2 = false;
+                    if (insertArray(&str, &colon, &brace1, &brace2, tokens, 0, &ni) == 0)
+                    {
+                        printf("Invalid input.\n");
+                        free(str);
+                        freeArray(cTokens);
+                        return EXIT_FAILURE;
+                    }
+                    if (!colon || !brace1 || !brace2 || ni > 32)
+                    {
+                        printf("Invalid input.\n");
+                        free(str);
+                        freeArray(cTokens);
+                        return EXIT_FAILURE;
+                    }
+                    tokens[0].size = ni;
+                    tokenSize += ni;
+                }
+                if (*str == 'W')
+                {
+                    inW = true;
+                    bool colon = false, brace1 = false, brace2 = false;
+                    if (insertArray(&str, &colon, &brace1, &brace2, tokens, 1, &wi) == 0)
+                    {
+                        printf("Invalid input.\n");
+                        free(str);
+                        freeArray(cTokens);
+                        return EXIT_FAILURE;
+                    };
+                    if (!colon || !brace1 || !brace2 || wi > 32)
+                    {
+                        printf("Invalid input.\n");
+                        free(str);
+                        freeArray(cTokens);
+                        return EXIT_FAILURE;
+                    }
+                    tokens[1].size = wi;
+                    tokenSize += wi;
+                }
+                if (*str == 'E')
+                {
+                    // printf(">%c<\n", *str);
+                    inE = true;
+                    bool colon = false, brace1 = false, brace2 = false;
+                    if (insertArray(&str, &colon, &brace1, &brace2, tokens, 2, &ei) == 0)
+                    {
+                        printf("Invalid input.\n");
+                        free(str);
+                        freeArray(cTokens);
+                        return EXIT_FAILURE;
+                    };
+                    if (!colon || !brace1 || !brace2 || ei > 32)
+                    {
+                        printf("Invalid input.\n");
+                        free(str);
+                        freeArray(cTokens);
+                        return EXIT_FAILURE;
+                    }
+                    tokens[2].size = ei;
+                    tokenSize += ei;
+                }
+                if (*str == 'S')
+                {
+                    inS = true;
+                    bool colon = false, brace1 = false, brace2 = false;
+                    if (insertArray(&str, &colon, &brace1, &brace2, tokens, 3, &si) == 0)
+                    {
+                        printf("Invalid input.\n");
+                        free(str);
+                        freeArray(cTokens);
+                        return EXIT_FAILURE;
+                    };
+                    if (!colon || !brace1 || !brace2 || si > 32)
+                    {
+                        printf("Invalid input.\n");
+                        free(str);
+                        freeArray(&tokens);
+                        return EXIT_FAILURE;
+                    }
+                    tokens[3].size = si;
+                    tokenSize += si;
+                }
+                // printf("outside: >%c<\n", *str);
+                str++;
+            }
+            // else
+            // {
+            //     printf("Invalid input.\n");
+            //     freeArray(cTokens);
+            //     free(str);
+            //     return EXIT_FAILURE;
+            //     break;
+            // }
+        }
+
+        str = NULL;
+    }
+    printf("Input ends\n");
+    if ((!inN || !inW || !inS || !inE))
+    {
+        printf("Invalid input.\n");
+        free(str);
+        freeArray(cTokens);
+        return EXIT_FAILURE;
+    }
 
     twoValue ab;
-    int ** answer = (int **) malloc(128 * sizeof(*answer));
-    for(size_t i = 0; i < 128; i++) {
-        answer[i] = (int *) malloc(2 * sizeof(int));
-    }
-    size_t ansI = 0;
-    initialSizes[0] = 2;
-    initialSizes[1] = 2;
-    initialSizes[2] = 4;
-    initialSizes[3] = 2;
-
-    ab = search(tokens, tokenSize, 0, 0, 1, answer, ansI);
-
-    
-    printf("Operations: %d\n", op);
-    printf("last: %d %d\n", ab.a, ab.b);
-    int a = 0, b = 0;
-    for(size_t i = 0; i < tokenSize; i++) {
-        if(i % 2) {
-            a+= (tokens[ab.answer[i][0]].arr[ab.answer[i][1]]);
+    int n[33], w[33], e[33], s[33];
+    size_t nni = 0, wni = 0, eni = 0, sni = 0;
+    for (size_t i = 0; i < 4; i++)
+    {
+        for (size_t j = 0; j < tokens[i].size; j++)
+        {
+            if (i == 0)
+                n[nni++] = tokens[i].arr[j];
+            if (i == 1)
+                w[wni++] = tokens[i].arr[j];
+            if (i == 2)
+                e[eni++] = tokens[i].arr[j];
+            if (i == 3)
+                s[sni++] = tokens[i].arr[j];
         }
-        else {
-            b+= (tokens[ab.answer[i][0]].arr[ab.answer[i][1]]);
+    }
+    // [0-3] [index];
+
+    size_t **answer = (size_t **)malloc(tokenSize * sizeof(*answer));
+    for (size_t i = 0; i < tokenSize; i++)
+    {
+        answer[i] = (size_t *)malloc(2 * sizeof(**answer));
+    }
+    nIni = tokens[0].size;
+    wIni = tokens[1].size;
+    eIni = tokens[2].size;
+    sIni = tokens[3].size;
+
+    size_t ansI = 0;
+    // for (size_t i = 0; i < 4; i++)
+    // {
+    //     for (size_t j = 0; j < tokens[i].size; j++)
+    //     {
+    //         printf("%d ", tokens[i].arr[j]);
+    //     }
+    //     printf("\n");
+    // }
+    ab = search(tokenSize, n, nni, w, wni, e, eni, s, sni, 0, 0, 1, answer, ansI);
+    
+    // 3 dim for storing the answer
+    printf("Operations: %d\n", op);
+    // printf("last: %d %d\n", ab.a, ab.b);
+    char letterMap[] = {'N', 'W', 'E', 'S'};
+    int a = 0, b = 0;
+    for (size_t i = 0; i < tokenSize; i++)
+    {
+        // printf("dir: %lu ind: %lu\n", answer[i][0], answer[i][1]);
+        // path check
+        if (i % 2 == 0)
+        {
+            a += tokens[answer[i][0]].arr[answer[i][1]];
+            printf("A: %c[%lu] (%d)\n", letterMap[answer[i][0]], answer[i][1], tokens[answer[i][0]].arr[answer[i][1]]);
+        }
+        else
+        {
+            b += tokens[answer[i][0]].arr[answer[i][1]];
+            printf("B: %c[%lu] (%d)\n", letterMap[answer[i][0]], answer[i][1], tokens[answer[i][0]].arr[answer[i][1]]);
         }
         printf("%d %d\n", ab.answer[i][0], ab.answer[i][1]);
     }
-    printf("path check: %d %d\n", a, b);
-    // for(size_t i = 0; i < 10; i++) {
-    //     free(tokens[i].arr);
-    // }
-    
-    // free(tokens);
+    printf("Total A/B: %d/%d\n", ab.a, ab.b);
+
+    for (size_t i = 0; i < tokenSize; i++)
+    {
+        free(answer[i]);
+    }
+    free(answer);
+    freeArray(&tokens);
 }
 
 // helper functions
-void removeNL(char * s) {
+void removeNL(char *s)
+{
     size_t len = strlen(s);
-    if (len > 0 && s[len-1] == '\n') { 
-        s[len-1] = '\0';
+    if (len > 0 && s[len - 1] == '\n')
+    {
+        s[len - 1] = '\0';
     }
 }
-
