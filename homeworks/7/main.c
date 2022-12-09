4261468a357b5649e534314a8e78e535a3462801
baljibil <baljibil@fit.cvut.cz>
2022-12-09 15:52:00 +0100

    hw07: 5.5 points edition

--- homeworks/7/main.c
+++ homeworks/7/main.c
@@ -8,74 +8,44 @@ typedef struct
     int *arr;
     int size;
 } ArrayWithSize;
+
 typedef struct
 {
     int a;
     int b;
-    int answer[128][2];
+    size_t answers[20][2];
 } twoValue;
-typedef struct {
-    int arr[32];
-    size_t size;
-} tokenArr;
-int maxInt(int a, int b) {
-    return (a > b) ? a : b;
-}
+
+
+void removeNL(char *s);
 int initialSizes[4];
 int op = 0;
-twoValue search(ArrayWithSize * tokens, size_t size, int a, int b, int aTurn, int ** answer, size_t ansI) {
+
+twoValue search(ArrayWithSize* tokens, int size, int a, int b, int aTurn, size_t **answer, size_t ansI)
+{
     op++;
+    // printf("a: %d b: %d\n",a,b);
    
-    twoValue ab;
-    ab.a = a;
-    ab.b = b;
     if (size == 0)
     {
-        printf("<>\n");
-        for(size_t i = 0; i < ansI; i++) {
-            printf("path %lu %lu\n", answer[i][0], answer[i][1]);
-            ab.answers[i][0] = answer[i][0];
-            ab.answers[i][1] = answer[i][1];
-        }
+        twoValue ab;
+        ab.a = a;
+        ab.b = b;
         return ab;
-    }
-    twoValue ab;
-
-    if (memoExists(memo, tokens, &ab))
-    {
-        // printf("memo exists: firstmover: %d secondmover: %d\n", ab.a, ab.b);
-        // for(size_t i = 0; i < cntl1; i++) {
-        //     printf("dir: %d, index: %d\n", leftOver1[i][0], leftOver1[i][1]);
-        // }
-        if (aTurn)
-        {
-            return ab;
-        }
-        else
-        {
-            int tmp = ab.a;
-            ab.a = ab.b;
-            ab.b = tmp;
-            return ab;
-        }
     }
     if (aTurn)
     {
-        twoValue arr[4];
-        twoValue dir[4];
-        int cnt = 0;
+        twoValue cur;
+        size_t cnt = 0;
         // copy tokens into tmp->
         twoValue max;
-        max.a = INT32_MIN;
         for(size_t i = 0; i < 4; i++) {
             if(tokens[i].size != 0) {
                 int current = tokens[i].arr[0];
-                answer[ansI][0] = i;
-                answer[ansI][1] = initialSizes[i]-tokens[i].size;
-
+                
                 tokens[i].size--;
                 tokens[i].arr++;
-                arr[cnt++] = search(tokens, size - 1, a + current, b, 0, answer, ansI+1);
+                cur = search(tokens, size - 1, a + current, b, 0, answer, ansI+1);
                 tokens[i].size++;
                 tokens[i].arr--;
                 if(cnt == 0) {
@@ -93,11 +63,11 @@ twoValue search(ArrayWithSize * tokens, size_t size, int a, int b, int aTurn, in
         return max;
     }
     else {
-        twoValue arr[4];
-        int cnt = 0;
+        twoValue cur;
+        size_t cnt = 0;
         // copy tokens into tmp->
         twoValue max;
-        max.b = INT32_MIN;
+        // if A have chosen the best, it's either the second best or the new element...
         for(size_t i = 0; i < 4; i++) {
             if(tokens[i].size != 0) {
                 int current = tokens[i].arr[0];
@@ -105,28 +75,27 @@ twoValue search(ArrayWithSize * tokens, size_t size, int a, int b, int aTurn, in
                 // answer[ansI][1] = initialSizes[i]-tokens[i].size;
                 tokens[i].size--;
                 tokens[i].arr++;
-                arr[cnt++] = search(tokens, size - 1, a , b + current, 1, answer, ansI+1);
+                cur = search(tokens, size - 1, a , b + current, 1, answer, ansI+1);
                 tokens[i].size++;
                 tokens[i].arr--;
-
-                if (cnt == 0)
-                {
+                
+                if(cnt == 0) {
                     max = cur;
                 }
-                if (max.b <= cur.b)
-                {
+                if(max.b <= cur.b) {
                     max = cur;
                     max.answers[ansI][0] = i;
-                    max.answers[ansI][1] = initialSizes[i] - tokens[i].size;
+                    max.answers[ansI][1] = initialSizes[i]-tokens[i].size;
                 }
                 cnt++;
+               
             }
         }
-        
-        pushToGlobalArray(memo, tokens, max.b - b, max.a - a);
+       
         return max;
     }
 }
+
 
 int insertArray(char **str, bool *colon, bool *brace1, bool *brace2, ArrayWithSize *tokens, int direction, int *index)
 {
@@ -162,8 +131,7 @@ int insertArray(char **str, bool *colon, bool *brace1, bool *brace2, ArrayWithSi
             continue;
         }
         // printf(">%c<\n", **str);
-        if (!isdigit(**str) && **str != '-')
-        {
+        if (!isdigit(**str) && **str != '-') {
             return 0;
         }
         if (isdigit(**str) || **str == '-')
@@ -221,9 +189,6 @@ void freeArray(ArrayWithSize **tokens)
     free((*tokens));
 }
 
-// N -> 0 W -> 1 E -> 2 S -> 3;
-// choices [ [[0,0], [0,1], [1,0]], [[0,0], [0,1], [1,0]], [[0,0], [0,1], [1,0]], [[0,0], [0,1], [1,0]]  ]
-
 int main()
 {
 
@@ -240,13 +205,12 @@ int main()
     int si = 0;
     int ei = 0;
 
-    int tokenSize = 0;
+    size_t tokenSize = 0;
     char *str = NULL;
     size_t size = 0;
     bool inN, inW, inE, inS = false;
     while (getline(&str, &size, stdin) != EOF)
     {
-        char ** strAddress = &str;        
         removeNL(str);
         while (*str == ' ')
         {
@@ -338,8 +302,7 @@ int main()
                 // printf("outside: >%c<\n", *str);
                 str++;
             }
-            else
-            {
+            else {
                 printf("Invalid input.\n");
                 freeArray(cTokens);
                 return EXIT_FAILURE;
@@ -362,73 +325,41 @@ int main()
         freeArray(cTokens);
         return EXIT_FAILURE;
     }
-
+    
     twoValue ab;
-    int n[33], w[33], e[33], s[33];
-    size_t nni = 0, wni = 0, eni = 0, sni = 0;
-    for (size_t i = 0; i < 4; i++)
-    {
-        for (size_t j = 0; j < tokens[i].size; j++)
-        {
-            if (i == 0)
-                n[nni++] = tokens[i].arr[j];
-            if (i == 1)
-                w[wni++] = tokens[i].arr[j];
-            if (i == 2)
-                e[eni++] = tokens[i].arr[j];
-            if (i == 3)
-                s[sni++] = tokens[i].arr[j];
-        }
+    
+    size_t **answer = (size_t **) malloc(sizeof(*answer) * 128);
+    for(size_t i = 0; i < 128; i++) {
+        answer[i] = (size_t *) malloc(2 * sizeof(size_t));
     }
-    // [0-3] [index];
 
-    // size_t **answer = (size_t **)malloc(tokenSize * sizeof(*answer));
-    // for (size_t i = 0; i < tokenSize; i++)
-    // {
-    //     answer[i] = (size_t *)malloc(2 * sizeof(**answer));
-    // }
-    size_t answer[128][2];
-    nIni = tokens[0].size;
-    wIni = tokens[1].size;
-    eIni = tokens[2].size;
-    sIni = tokens[3].size;
+    initialSizes[0] = tokens[0].size;
+    initialSizes[1] = tokens[1].size;
+    initialSizes[2] = tokens[2].size;
+    initialSizes[3] = tokens[3].size;
 
     size_t ansI = 0;
-    // for (size_t i = 0; i < 4; i++)
-    // {
-    //     for (size_t j = 0; j < tokens[i].size; j++)
-    //     {
-    //         printf("%d ", tokens[i].arr[j]);
-    //     }
-    //     printf("\n");
-    // }
-    ab = search(tokenSize, n, nni, w, wni, e, eni, s, sni, 0, 0, 1, answer, ansI);
-    // 3 dim for storing the answer
-    // printf("Operations: %d\n", op);
-    // printf("last: %d %d\n", ab.a, ab.b);
+  
+    ab = search(tokens, tokenSize, 0, 0, 1, answer, ansI);
     char letterMap[] = {'N', 'W', 'E', 'S'};
-    int a = 0, b = 0;
+    // int a = 0, b = 0;
     for (size_t i = 0; i < tokenSize; i++)
     {
-        // printf("dir: %lu ind: %lu\n", answer[i][0], answer[i][1]);
-        // path check
         if (i % 2 == 0)
         {
-            a += tokens[answer[i][0]].arr[answer[i][1]];
-            printf("A: %c[%lu] (%d)\n", letterMap[answer[i][0]], answer[i][1], tokens[answer[i][0]].arr[answer[i][1]]);
+            // a += tokens[ab.answers[i][0]].arr[ab.answers[i][1]];
+            printf("A: %c[%lu] (%d)\n", letterMap[ab.answers[i][0]], ab.answers[i][1], tokens[ab.answers[i][0]].arr[ab.answers[i][1]]);
         }
         else
         {
-            b += tokens[answer[i][0]].arr[answer[i][1]];
-            printf("B: %c[%lu] (%d)\n", letterMap[answer[i][0]], answer[i][1], tokens[answer[i][0]].arr[answer[i][1]]);
+            // b += tokens[ab.answers[i][0]].arr[ab.answers[i][1]];
+            printf("B: %c[%lu] (%d)\n", letterMap[ab.answers[i][0]], ab.answers[i][1], tokens[ab.answers[i][0]].arr[ab.answers[i][1]]);
         }
-        printf("%d %d\n", ab.answer[i][0], ab.answer[i][1]);
     }
     printf("Total A/B: %d/%d\n", ab.a, ab.b);
-    printf("Path check: %d/%d\n", a,b);
-
+    // printf("Path check: %d/%d\n", a,b);
     
-    freeArray(&tokens);
+    freeArray(cTokens);
 }
 
 // helper functions
